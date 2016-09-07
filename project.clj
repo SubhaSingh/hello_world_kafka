5d6b2a5f9fda730494272df5be8326811c598ff7
Subha <singh.subha22@gmail.com>
2016-09-07 20:48:49 +0200

    Producer code.

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject hello_world_kafka "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Create a kafka producer and high level consumer"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.kafka/kafka_2.9.2 "0.8.1.1" :exclusions [javax.jms/jms
+                                                                      com.sun.jdmk/jmxtools
+                                                                      com.sun.jmx/jmxri]]]
+
+  :aot [hello-world-kafka.core]
+  :main hello-world-kafka.core
+  )
--- src/hello_world_kafka/core.clj
+++ src/hello_world_kafka/core.clj
@@ -1,6 +1,23 @@
-(ns hello-world-kafka.core)
+(ns hello-world-kafka.core
+  (:import (kafka.consumer Consumer ConsumerConfig KafkaStream)
+           (kafka.producer KeyedMessage ProducerConfig)
+           (kafka.javaapi.producer Producer)
+           (java.util Properties)
+           (java.util.concurrent Executors))
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- create-producer
+  "Creates a producer that can be used to send a message to Kafka"
+  [brokers]
+  (let [props (Properties.)]
+    (doto props
+      (.put "metadata.broker.list" brokers)
+      (.put "serializer.class" "kafka.serializer.StringEncoder")
+      (.put "request.required.acks" "1"))
+    (Producer. (ProducerConfig. props))))
+
+(defn- send-to-producer
+  "Send a string message to Kafka"
+  [producer topic message]
+  (let [data (KeyedMessage. topic nil message)]
+    (.send producer data)))
